85950ffa85fdedf55f53e0945e36268bf6704e0d
tao <xdd666@foxmail.com>
2020-07-28 16:57:52 +0800

    项目结构调整

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:book_web/widget/page/main/page.dart';
 import 'package:fish_redux/fish_redux.dart';
 import 'package:flutter/cupertino.dart' hide Page;
 import 'package:flutter/material.dart' hide Page;
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 void main() {
   runApp(createApp());
@@ -16,7 +17,7 @@ Widget createApp() {
   );
 
   return MaterialApp(
-    title: 'Book Backstage',
+    title: 'Flutter Book Backstage',
     theme: ThemeData(
       primarySwatch: Colors.blue,
     ),
@@ -26,14 +27,28 @@ Widget createApp() {
     onGenerateRoute: (RouteSettings settings) {
       //ios页面切换风格
       return CupertinoPageRoute(builder: (BuildContext context) {
+        autoUi(context);  //界面适配
         return routes.buildPage(settings.name, settings.arguments);
       });
 
       //Material页面切换风格
 //      return MaterialPageRoute<Object>(builder: (BuildContext context) {
-//        uiAdapter(context);  //界面适配
+//        autoUi(context);  //界面适配
 //        return routes.buildPage(settings.name, settings.arguments);
 //      });
     },
   );
+}
+
+
+//界面适配
+void autoUi(BuildContext context) {
+  //填入设计稿中设备的屏幕尺寸
+  //默认 width : 1080px , height:1920px , allowFontScaling:false
+//    ScreenUtil.init(context);
+  //假如设计稿是按iPhone6的尺寸设计的(iPhone6 750*1334)
+//    ScreenUtil.init(context, width: 750, height: 1334);
+  //设置字体大小根据系统的“字体大小”辅助选项来进行缩放,默认为false
+//    ScreenUtil.init(context, width: 750, height: 1334, allowFontScaling: true);
+  ScreenUtil.init(context, width: 750, height: 1334, allowFontScaling: true);
 }
--- lib/widget/page/main/view.dart
+++ lib/widget/page/main/view.dart
@@ -1,3 +1,4 @@
+import 'package:book_web/utils/auto_ui.dart';
 import 'package:book_web/widget/page/main/action.dart';
 import 'package:fish_redux/fish_redux.dart';
 import 'package:flutter/material.dart';
@@ -11,7 +12,7 @@ Widget buildView(MainState state, Dispatch dispatch, ViewService viewService) {
   _state = state;
   _dispatch = dispatch;
 
-  return Scaffold(appBar: AppBar(title: Text("主页面")), body: totalPage());
+  return Scaffold(body: totalPage());
 }
 
 //构建总体页面
@@ -33,14 +34,32 @@ Widget totalPage() {
 //增加NavigationRail组件为侧边栏
 Widget navigationRailSide() {
   //顶部widget
-  Widget topWidget = FloatingActionButton.extended(
-    icon: Icon(Icons.navigation),
+  Widget topWidget = Center(
+    child: Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Container(
+          width: auto(80),
+          height: auto(80),
+          decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            image: DecorationImage(
+                image: NetworkImage(
+                    "https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=3383029432,2292503864&fm=26&gp=0.jpg"),
+                fit: BoxFit.fill),
+          )),
+    ),
   );
 
-
   //底部widget
-  Widget bottomWidget = Text("........");
-
+  Widget bottomWidget = Container(
+    padding: EdgeInsets.all(0),
+    child: FloatingActionButton(
+      onPressed: () {
+        _dispatch(MainActionCreator.switchExtended(!_state.isExtended));
+      },
+      child: Icon(_state.isExtended ? Icons.near_me : Icons.navigation),
+    ),
+  );
 
   return NavigationRail(
     //阴影Z轴高度
@@ -62,12 +81,11 @@ Widget navigationRailSide() {
     ],
     //顶部widget
     leading: topWidget,
-    //底部widget
+//    //底部widget
     trailing: bottomWidget,
     selectedIndex: _state.selectedIndexRail,
     onDestinationSelected: (int index) {
       _dispatch(MainActionCreator.switchTab(index));
-      _dispatch(MainActionCreator.switchExtended(!_state.isExtended));
     },
   );
 }
