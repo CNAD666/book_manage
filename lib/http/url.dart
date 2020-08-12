c0e5c4690b5f5ef9c5e10f6a52018718b8ea1cf7
XDD666 <35656085+CNAD666@users.noreply.github.com>
2020-08-12 23:14:44 +0800

    首页样式变动

--- lib/http/url.dart
+++ lib/http/url.dart
@@ -1,3 +1,8 @@
 
 
-const String PIC_HOME_BG = "https://cdn.jsdelivr.net/gh/CNAD666/MyData/pic/flutter/bookWeb/20200804225842.jpg";
+class ImageUrl{
+  //背景图
+  static const String PIC_HOME_BG = "https://cdn.jsdelivr.net/gh/CNAD666/MyData/pic/flutter/bookWeb/20200804225842.jpg";
+}
+
+
--- lib/widget/page/home/home_view.dart
+++ lib/widget/page/home/home_view.dart
@@ -1,34 +1,39 @@
 import 'package:book_web/http/url.dart';
-import 'package:book_web/utils/keep_alive_page.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Stack(
-        fit: StackFit.expand,
-        children: [
-          Image.network(
-            PIC_HOME_BG,
-            fit: BoxFit.fill,
-          ),
-          body(),
-        ],
-      ),
+    return Stack(
+      fit: StackFit.expand,
+      children: [
+        _pageBg(),
+        _body(),
+      ],
     );
   }
 }
 
+///页面背景图
+Widget _pageBg() {
+  return Image.network(
+    ImageUrl.PIC_HOME_BG,
+    fit: BoxFit.fill,
+  );
+}
 
-Widget body() {
-  return Stack(
-    children: [
-      Container(
-        width: 100,
-        height: 100,
-        color: Colors.lightBlueAccent,
-      ),
-    ],
+///主体body模块
+Widget _body() {
+  return Container(
+    alignment: Alignment.center,
+    child:Stack(
+      children: [
+        Container(
+          width: 1000,
+          height: 500,
+          color: Color(0x9FFFFFFF),
+        ),
+      ],
+    ),
   );
 }
