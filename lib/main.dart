4f7279c7b00c3e9629d3ed7be33aa62ea6aa4a8a
XDD666 <35656085+CNAD666@users.noreply.github.com>
2020-08-22 23:55:49 +0800

    微调

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
-import 'package:fluro/fluro.dart' ;
-import 'package:flutter/material.dart' hide Page,Router;
-import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:fluro/fluro.dart';
+import 'package:flutter/material.dart' hide Page, Router;
 
 import 'app/routes/application.dart';
 import 'app/routes/routes.dart';
@@ -23,16 +22,4 @@ class MyApp extends StatelessWidget {
       home: HomePage(),
     );
   }
-}
-
-//界面适配
-void autoUi(BuildContext context) {
-  //填入设计稿中设备的屏幕尺寸
-  //默认 width : 1080px , height:1920px , allowFontScaling:false
-//    ScreenUtil.init(context);
-  //假如设计稿是按iPhone6的尺寸设计的(iPhone6 750*1334)
-//    ScreenUtil.init(context, width: 750, height: 1334);
-  //设置字体大小根据系统的“字体大小”辅助选项来进行缩放,默认为false
-//    ScreenUtil.init(context, width: 750, height: 1334, allowFontScaling: true);
-  ScreenUtil.init(context, width: 750, height: 1334, allowFontScaling: true);
 }
