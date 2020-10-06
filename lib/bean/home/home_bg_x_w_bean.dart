37fe7012853240b2471b0fd4e70664c71036e8a9
XDD666 <35656085+CNAD666@users.noreply.github.com>
2020-10-06 23:00:25 +0800

    调整json结构

--- lib/bean/home/home_bg_x_w_bean.dart
+++ lib/bean/home/home_bg_x_w_bean.dart
@@ -1,8 +1,8 @@
 import 'package:book_web/generated/json/base/json_convert_content.dart';
 
 class HomeBgXWBean with JsonConvert<HomeBgXWBean> {
-	String code;
-	String imgurl;
-	String width;
-	String height;
+  String code;
+  String imgurl;
+  String width;
+  String height;
 }
